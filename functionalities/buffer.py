558e1583d2bce31c920bf4334ac446b7ff32c7fc
mmackowsky <maciostah@gmail.com>
2023-04-27 22:03:50 +0200

    feat(buffer): add final structure of file

--- functionalities/buffer.py
+++ functionalities/buffer.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 
 @dataclass
@@ -8,34 +9,31 @@ class Text:
     rot_type: str
     status: str
 
+    def __str__(self):
+        return f"Text: {self.txt}, result: {self.result}, ROT: {self.rot_type}. Status: {self.status}"
+
     def __repr__(self):
-        return f"[text={self.txt}, result={self.result}, rot_type={self.rot_type}, status={self.status}]"
+        return f"Text(text={self.txt}, result={self.result}, rot_type={self.rot_type}, status={self.status})"
 
 
 class Buffer:
-    BUFFER_MENU = {"1.": "Show", "2.": "Remove element", "3.": "Return"}
-
-    data = []
-
-    def show_buffer_menu(self) -> None:
-        for key, value in self.BUFFER_MENU.items():
-            print(key, value)
-
-    def show(self):
-        if not self.data:
-            print("Buffer is empty.")
-        for count, element in enumerate(self.data):
-            print(count, element)
+    data: List[Text] = []
 
     @staticmethod
-    def add(value: Text):
+    def add(value: Text) -> None:
         Buffer.data.append(value)
 
-    def remove_element(self):
+    def show(self) -> None:
+        if not self.data:
+            print("Buffer is empty.")
+        for count, element in enumerate(self.data, start=1):
+            print(count, element)
+
+    def remove_element(self) -> None:
         while True:
             try:
-                element_to_remove = int(
-                    input("Type number of element you want to remove: ")
+                element_to_remove = (
+                    int(input("Type number of element you want to remove: ")) - 1
                 )
                 del self.data[element_to_remove]
                 break
@@ -44,5 +42,4 @@ class Buffer:
                 back = input("If you want back just type 'back', else press ENTER: ")
                 if back == "back":
                     break
-                else:
-                    continue
+                continue
